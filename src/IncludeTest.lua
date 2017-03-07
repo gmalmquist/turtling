ff8a8297bf6ecd8c0bac44b48cc74a05ba6706c0
Garrett Malmquist <garrett.malmquist@gmail.com>
2017-03-07 10:52:50 -0500

    GitRun itself now processes includes dynamically.
    Now there's no need for the install script to preprocess things.

---
+++ src/IncludeTest.lua
@@ -0,0 +1,5 @@
+print("Trying to include 'Hello.lua'.")
+
+#include Hello.lua
+
+print("Did it work?")
--- src/universal/GitRun.lua
+++ src/universal/GitRun.lua
@@ -6,18 +6,18 @@ function load_properties()
   if not fs.exists(PROPERTIES_FILE) then
     return {}
   end
-  h = fs.open(PROPERTIES_FILE, "r")
-  text = h.readAll()
+  local h = fs.open(PROPERTIES_FILE, "r")
+  local text = h.readAll()
   h.close()
-  properties = {}
+  local properties = {}
   while true do
-    eq = string.find(text, "=")
+    local eq = string.find(text, "=")
     if eq == nil then
       break
     end
-    key = string.sub(text, 1, eq - 1)
-    newline = string.find(text, "\n")
-    val = ""
+    local key = string.sub(text, 1, eq - 1)
+    local newline = string.find(text, "\n")
+    local val = ""
     if newline == nil then
       val = string.sub(text, eq + 1)
       properties[key] = val
@@ -33,19 +33,106 @@ end
 
 
 function save_properties(properties)
-  h = fs.open(PROPERTIES_FILE, "w")
+  local h = fs.open(PROPERTIES_FILE, "w")
   for k, v in pairs(properties) do
     h.write(string.format("%s=%s\n", k, v))
   end
   h.close()
 end
 
+
+function process_download(properties, out)
+  local included = properties.included or {}
+
+  function get_url(properties)
+    if properties["url"] ~= nil and string.len(properties["url"]) > 0 then
+      return properties["url"]
+    end
+    local dir = properties.dir
+    local path = properties.path
+    if string.len(dir) > 0 then
+      path = string.format("%s/%s", dir, path)
+    end
+    return string.format(
+      "https://raw.githubusercontent.com/%s/%s/%s/%s",
+      properties.org,
+      properties.repo,
+      properties.branch,
+      path
+    )
+  end
+
+  function process_line(line)
+    local a, b = string.find(line, '#include')
+    if a ~= nil then
+      -- process file inclusion
+      local path = string.sub(line, b + 1)
+      while string.find(path, " ") == 1 do
+        path = string.sub(path, 2)
+      end
+      while string.sub(string.len(path), string.len(path)) == " " do
+        path = string.sub(path, 1, string.len(path) - 1)
+      end
+      local url = ""
+      if string.find(path, "http:") == 1 or string.find(path, "https:") == 1 then
+        url = path
+      end
+      local dir = properties.dir
+      if string.find(path, "/") == 1 then
+        dir = ""
+        path = string.sub(path, 2)
+      end
+      return process_download({
+        org = properties.org,
+        repo = properties.repo,
+        branch = properties.branch,
+        dir = dir,
+        path = path,
+        url = url,
+        included = included
+      }, out)
+    end
+    out.write('\n')
+    out.write(line)
+    return true
+  end
+
+  local url = get_url(properties)
+  if url == nil or string.len(url) == 0 then
+    print("Cannot download nil or '' url.")
+    return false
+  end
+  if included[url] then
+    return true -- We already included this file.
+  end
+  included[url] = true
+
+  local h = http.get(url)
+  if h ~= nil then
+    local line = h.readLine()
+    while line ~= nil do
+      if not process_line(line) then
+        print(string.format("Unable to process: %s", line))
+        h.close()
+        return false
+      end
+      line = h.readLine()
+    end
+    h.close()
+    return true
+  else
+    print(string.format("404 - %s", url))
+    return false
+  end
+end
+
+
 function download_script(properties)
-  org = properties["org"] or "gmalmquist"
-  repo = properties["repo"] or "turtling"
-  branch = properties["branch"] or "master"
-  dir = properties["dir"] or ""
-  path = properties["path"] or ""
+  local org = properties["org"] or "gmalmquist"
+  local repo = properties["repo"] or "turtling"
+  local branch = properties["branch"] or "master"
+  local dir = properties["dir"] or ""
+  local path = properties["path"] or ""
 
   print(string.format("org=%s", org))
   print(string.format("repo=%s", repo))
@@ -53,18 +140,14 @@ function download_script(properties)
   print(string.format("dir=%s", dir))
   print(string.format("path=%s", path))
 
-  if string.len(dir) > 0 then
-    path = string.format("%s/%s", dir, path)
-  end
-
-  target_name = path
+  local target_name = path
   -- Strip out extension.
-  dot_lua = string.find(target_name, ".lua")
+  local dot_lua = string.find(target_name, ".lua")
   if dot_lua ~= nil then
     target_name = string.sub(target_name, 1, dot_lua - 1)
   end
   -- Strip out directory component.
-  last_slash = string.find(target_name, "/")
+  local last_slash = string.find(target_name, "/")
   while last_slash ~= nil do
     target_name = string.sub(target_name, last_slash + 1)
     last_slash = string.find(target_name, "/")
@@ -76,28 +159,18 @@ function download_script(properties)
       return
     end
 
-    url = string.format(
-      "https://raw.githubusercontent.com/%s/%s/%s/%s",
-      org,
-      repo,
-      branch,
-      path
-    )
-
-    print(url)
     print(string.format("Downloading to '%s' ...", target_name))
-
-    h = http.get(url)
-    if h ~= nil then
-      content = h.readAll()
-      h.close()
-
-      h = fs.open(target_name, "w")
-      h.write(content)
-      h.close()
+    local h = fs.open(target_name, "w")
+    local result = process_download({
+      org = org,
+      repo = repo,
+      branch = branch,
+      dir = dir,
+      path = path
+    }, h)
+    h.close()
+    if result then
       return target_name
-    else
-      print("404")
     end
   else
     print(string.format("Could not infer destination file from '%s'.", path))
